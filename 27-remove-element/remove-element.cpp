57d305ef2800b36a956a744b0f8b7f413fc9a999
mohdfazal0624-rgb <mohdfazal0624@gmail.com>
2026-06-16 13:54:28 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (83.58%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+    
+        int a=0;
+        for (int i=0;i<nums.size();i++){
+
+            if(val!=nums[i]){
+            nums[a]=nums[i];
+            a++;
+            
+            }
+        }
+        return a;
+    }
+};
