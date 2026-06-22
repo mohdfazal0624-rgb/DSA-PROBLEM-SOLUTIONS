446cd1a0bea5ffe1ad18e3a75dd2b03e0b55038d
mohdfazal0624-rgb <mohdfazal0624@gmail.com>
2026-06-22 21:15:45 +0530

    Time: 48 ms (58.71%) | Memory: 61.8 MB (37.85%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+int countballs(vector<int>& arr,int middy){
+     int ball=1;
+     
+     
+     
+     
+     int force=arr[0];
+     for(int i=1;i<arr.size();i++){
+        if ((arr[i]-force)>=middy){
+            ball++;
+            force=arr[i];
+        }
+        else continue;
+
+     }
+     return ball;
+
+}
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
+        int st=1;
+        int end=position[position.size()-1]-position[0];
+        int ans=end;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(countballs(position,mid)>=m){
+                ans=mid;
+                st=mid+1;
+            }
+            else 
+            end=mid-1;
+        }
+        return ans ;
+    }
+};
