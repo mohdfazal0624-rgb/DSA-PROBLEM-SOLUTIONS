ca4197adb3740a625cddef2248f143172eecf1b5
mohdfazal0624-rgb <mohdfazal0624@gmail.com>
2026-06-24 21:17:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (13.10%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int curr=0;
+       for(int i=0;i<nums.size();i++){
+        curr+=nums[i];
+        nums[i]=curr;
+
+       } 
+       return nums;
+    }
+};
