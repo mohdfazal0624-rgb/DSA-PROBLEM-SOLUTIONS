6be47f7768fdfb0a7e35df47241db62245a818ac
mohdfazal0624-rgb <mohdfazal0624@gmail.com>
2026-07-02 16:28:40 +0530

    Time: 29 ms (62.66%) | Memory: 29.4 MB (52.16%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -2,23 +2,36 @@ class Solution {
 public:
     string compressedString(string word) {
         string comp = "";
-        int c = 1;
+        int f = 1;
+        int j = 0;
 
-        for (int i = 1; i <= word.size(); i++) {
+        for (int i = 1; i < word.size(); i++) {
+            if (word[i] == word[j]) {
+                f++;
+            } else {
+                while (f > 9) {
+                    comp += to_string(9);
+                    comp += word[j];
+                    f -= 9;
+                }
 
-           
-            if (i < word.size() && word[i] == word[i - 1] && c < 9) {
-                c++;
-            }
-            else {
-                
-                comp += to_string(c);
-                comp += word[i - 1];
+                comp += to_string(f);
+                comp += word[j];
 
-                
-                c = 1;
+                j = i;
+                f = 1;
             }
         }
+
+        // Process the last group
+        while (f > 9) {
+            comp += to_string(9);
+            comp += word[j];
+            f -= 9;
+        }
+
+        comp += to_string(f);
+        comp += word[j];
 
         return comp;
     }
