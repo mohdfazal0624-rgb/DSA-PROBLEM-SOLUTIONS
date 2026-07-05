97eb4543eccada8be29c812b90451c17feaea18d
mohdfazal0624-rgb <mohdfazal0624@gmail.com>
2026-07-05 20:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.64%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-vector<int> fs(26,0);
-vector<int> ft(26,0);
-for(int i=0;i<s.length();i++){
-     fs[s[i]-'a']++;
-}
-for(int i=0;i<t.length();i++){
-     ft[t[i]-'a']++;}
-     for(int i=0;i<26;i++){
-        if(fs[i]!=ft[i])
-        return false;
-     }
-  return true;
-  
+        if (s.length() != t.length())
+            return false;
 
+        vector<int> fs(26, 0);
+        vector<int> ft(26, 0);
+
+        for (int i = 0; i < s.length(); i++) {
+            fs[s[i] - 'a']++;
+            ft[t[i] - 'a']++;
+        }
+
+        return fs == ft;
     }
 };
