9416646fa47dc89284548c01fd5714ca629b6f63
mohdfazal0624-rgb <mohdfazal0624@gmail.com>
2026-07-06 20:26:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (25.74%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -6,24 +6,16 @@ public:
         if (s.length() != t.length())
             return false;
 
-        unordered_map<char, int> m;
-        unordered_map<char, int> n;
+      
+
+        vector<int> fs(26, 0);
+        vector<int> ft(26, 0);
 
         for (int i = 0; i < s.length(); i++) {
-            m[s[i]]++;
-            n[t[i]]++;
-        }
-
-        return m == n;
-    }
-};
-       // vector<int> fs(26, 0);
-        //vector<int> ft(26, 0);
-
-        //for (int i = 0; i < s.length(); i++) {
-          //  fs[s[i] - 'a']++;
-            //ft[t[i] - 'a']++;
+           fs[s[i] - 'a']++;
+        ft[t[i] - 'a']++;}
         
 
-        //return fs == ft;
-
+        return fs == ft;
+        }
+    };
