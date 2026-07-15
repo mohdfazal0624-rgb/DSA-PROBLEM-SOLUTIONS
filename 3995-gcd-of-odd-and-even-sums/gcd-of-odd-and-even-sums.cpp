835c424999084329d030354e797f0649e8aa71be
MD_Fazzal <mohdfazal0624@gmail.com>
2026-07-15 23:23:47 +0530

    Time: 574 ms (5.57%) | Memory: 8.4 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -3,16 +3,14 @@ public:
     int gcdOfOddEvenSums(int n) {
         int evesum=0;
         int oddsum=0;
-        int gcd=1;
+    int gcd=1;
         evesum=n*(n+1);
         oddsum=evesum-n;
-    for(int i=1;i<=oddsum;i++){
-        if(evesum%i==0&&oddsum%i==0){
-           gcd=i;
-        }
-    }
-    return gcd;
-                
-        
+     for(int i=1;i<=oddsum;i++){
+         if(evesum%i==0&&oddsum%i==0){
+            gcd=i;}
+     }
+     return gcd;
+   
             }
 };
