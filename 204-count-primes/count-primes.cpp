981995711b1b58ffb43edbcbf18fb80ce697bca0
MD_Fazzal <mohdfazal0624@gmail.com>
2026-07-15 23:33:35 +0530

    Time: 242 ms (19.23%) | Memory: 12.6 MB (64.77%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+      vector<bool> isprime (n+1,true);
+      int ans=0;
+      for(int i=2;i<n;i++){
+        if(isprime[i]){
+            ans++;
+            for(int j=i*2;j<n;j=j+i){
+                isprime[j]=false;
+            }
+        }
+
+      }
+      return ans;   
+    }
+};
