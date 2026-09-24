6a596cb218828662300aaf59274741155f29acfe
MD_Fazzal <mohdfazal0624@gmail.com>
2026-09-25 01:25:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (53.58%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        string c="";
-        for(int i=0;i<s.length();i++){
-        c+= tolower(s[i]);
-        }
-        return c;
+        // brute force 
+    //  string c="";
+    //      for(int i=0;i<s.length();i++){
+    //      c+= tolower(s[i]);
+    //      }
+    //      return c;
+      
+    //    return c;
+
+    // optimize
+     transform(s.begin(),s.end(),s.begin(),::tolower);
+     return s;
     }
 };
