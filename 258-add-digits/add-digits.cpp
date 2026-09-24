8f03684e785303c9bd8c36077a108e208d34a7fd
MD_Fazzal <mohdfazal0624@gmail.com>
2026-09-25 01:14:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (78.94%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
 // bruteforce
-int sum(int a){
-    int sum=0;
-    while(a!=0){
-        sum+=a%10;
-        a=a/10;
-    }
-    return sum;
-}
-    int addDigits(int num) {
+//int sum(int a){
+//     int sum=0;
+//     while(a!=0){
+//         sum+=a%10;
+//         a=a/10;
+//     }
+//     return sum;
+// }
+     int addDigits(int num) {
         
-        while(num>9){
-            num=sum(num);
-        }
-        return num;
+//         while(num>9){
+//             num=sum(num);
+//         }
+//         return num;
+if(num==0)
+return 0;
+
+return 1+(num-1)%9;
         
     }
 };
